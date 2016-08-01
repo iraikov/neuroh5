19c4e539d2ca36d4e548e6e0b1883734e33651f0
gheber <gheber@jelly.ad.hdfgroup.org>
2016-08-01 10:32:01 -0500

    First draft of a parallel reader.

---
+++ reader/ngh5types.h
@@ -0,0 +1,20 @@
+#ifndef NGH5TYPES_HH
+#define NGH5TYPES_HH
+
+#include "hdf5.h"
+
+// node index type
+
+typedef unsigned int NODE_IDX_T;
+
+#define NODE_IDX_H5_NATIVE_T H5T_NATIVE_UINT32
+#define NODE_IDX_H5_FILE_T   H5T_STD_U32LE
+
+// CRS offset type
+
+typedef uint64_t ROW_PTR_T;
+
+#define ROW_PTR_H5_NATIVE_T H5T_NATIVE_UINT64
+#define ROW_PTR_H5_FILE_T   H5T_STD_U64LE
+
+#endif
