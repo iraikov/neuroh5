7238936d2d7818116e0bbd765d23cfb9d4d2b22e
Gerd Heber <gheber@hdfgroup.org>
2016-08-16 14:36:35 -0500

    Renamed file.

---
+++ reader/ngh5types.hh
@@ -0,0 +1,47 @@
+ #ifndef NGH5TYPES_HH
+#define NGH5TYPES_HH
+
+#include <map>
+
+#include "hdf5.h"
+
+// node index type
+
+typedef unsigned int NODE_IDX_T;
+
+#define NODE_IDX_H5_NATIVE_T H5T_NATIVE_UINT32
+#define NODE_IDX_H5_FILE_T   H5T_STD_U32LE
+
+// CRS offset type
+
+typedef uint64_t ROW_PTR_T;
+
+#define ROW_PTR_H5_NATIVE_T H5T_NATIVE_UINT64
+#define ROW_PTR_H5_FILE_T   H5T_STD_U64LE
+
+// Population types
+
+// memory type for valid population combinations
+
+typedef uint16_t pop_t;
+
+typedef struct
+{
+  uint16_t src;
+  uint16_t dst;
+}
+pop_comb_t;
+
+typedef struct
+{
+  uint64_t start;
+  uint32_t count;
+  uint16_t pop;
+}
+pop_range_t;
+
+typedef std::map<NODE_IDX_T,std::pair<uint32_t,pop_t> > pop_range_map_t;
+
+typedef pop_range_map_t::const_iterator pop_range_iter_t;
+
+#endif
