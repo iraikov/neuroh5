4505f649d6e6400fbb67474486c9a606eab8b720
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-08-30 18:02:33 -0700

    added a conditional to create_edge_list to prevent stepping over the last element of dst_ptr

--- reader/main.cc
+++ reader/main.cc
@@ -35,23 +35,27 @@ int create_edge_list
  vector<NODE_IDX_T>&       edge_list
  )
 {
-  int ierr = 0;
-
+  int ierr = 0; size_t dst_ptr_size;
+  
   if (dst_blk_ptr.size() > 0) 
     {
+      dst_ptr_size = dst_ptr.size();
       for (size_t b = 0; b < dst_blk_ptr.size()-1; ++b)
         {
           size_t low_dst_ptr = dst_blk_ptr[b], high_dst_ptr = dst_blk_ptr[b+1];
           NODE_IDX_T dst_base = base + dst_idx[b];
           for (size_t i = low_dst_ptr, ii = 0; i < high_dst_ptr; ++i, ++ii)
             {
-              NODE_IDX_T dst = dst_base + ii + dst_start;
-              size_t low = dst_ptr[i], high = dst_ptr[i+1];
-              for (size_t j = low; j < high; ++j)
+              if (i < dst_ptr_size-1) 
                 {
-                  NODE_IDX_T src = src_idx[j] + src_start;
-                  edge_list.push_back(src);
-                  edge_list.push_back(dst);
+                  NODE_IDX_T dst = dst_base + ii + dst_start;
+                  size_t low = dst_ptr[i], high = dst_ptr[i+1];
+                  for (size_t j = low; j < high; ++j)
+                    {
+                      NODE_IDX_T src = src_idx[j] + src_start;
+                      edge_list.push_back(src);
+                      edge_list.push_back(dst);
+                    }
                 }
             }
         }
@@ -77,13 +81,12 @@ int main(int argc, char** argv)
 
   if (argc < 2) 
     {
-      std::cout << "Usage: reader <FILE> <DATASET>" << std::endl;
+      std::cout << "Usage: reader <FILE> <DATASET> ..." << std::endl;
       exit(1);
     }
 
  
   // read the population info
-
   set< pair<pop_t, pop_t> > pop_pairs;
   assert(read_population_combos(MPI_COMM_WORLD, argv[1], pop_pairs) >= 0);
 
@@ -91,24 +94,26 @@ int main(int argc, char** argv)
   map<NODE_IDX_T,pair<uint32_t,pop_t> > pop_ranges;
   assert(read_population_ranges(MPI_COMM_WORLD, argv[1], pop_ranges, pop_vector) >= 0);
 
-  // read the edges
-
-  NODE_IDX_T base, dst_start, src_start;
-  vector<DST_BLK_PTR_T> dst_blk_ptr;
-  vector<NODE_IDX_T> dst_idx;
-  vector<DST_PTR_T> dst_ptr;
-  vector<NODE_IDX_T> src_idx;
-  assert(read_dbs_projection(MPI_COMM_WORLD, argv[1], argv[2], pop_vector, base, dst_start, src_start, dst_blk_ptr, dst_idx, dst_ptr, src_idx) >= 0);
-
-  // validate the edges
-
-  //assert(validate_edge_list(base, row_ptr, col_idx, pop_ranges, pop_pairs) == true);
-
-  // create the partitioner input
-
   vector<NODE_IDX_T> edge_list;
-  assert(create_edge_list(base, dst_start, src_start, dst_blk_ptr, dst_idx, dst_ptr, src_idx, edge_list) >= 0);
 
+  // read the edges
+  for (int i = 0; i < argc-2; i++)
+    {
+      NODE_IDX_T base, dst_start, src_start;
+      vector<DST_BLK_PTR_T> dst_blk_ptr;
+      vector<NODE_IDX_T> dst_idx;
+      vector<DST_PTR_T> dst_ptr;
+      vector<NODE_IDX_T> src_idx;
+      assert(read_dbs_projection(MPI_COMM_WORLD, argv[1], argv[i+2], pop_vector, base, dst_start, src_start, dst_blk_ptr, dst_idx, dst_ptr, src_idx) >= 0);
+      
+      // validate the edges
+      
+      //assert(validate_edge_list(base, row_ptr, col_idx, pop_ranges, pop_pairs) == true);
+      
+      // create the partitioner input
+      assert(create_edge_list(base, dst_start, src_start, dst_blk_ptr, dst_idx, dst_ptr, src_idx, edge_list) >= 0);
+    }
+  
   if (edge_list.size() > 0) 
     {
       assert(edge_list.size()%2 == 0);
@@ -117,6 +122,7 @@ int main(int argc, char** argv)
           std::cout << k << " " << edge_list[i] << " " << edge_list[i+1] << std::endl;
         }
     }
+
   MPI_Barrier(MPI_COMM_WORLD);
   //MPI_Finalize();
   return 0;
