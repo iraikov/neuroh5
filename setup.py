d822fe62d820f3faf360f4da873370039564fb63
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-10-21 11:11:54 -0700

    adding together python setup files for reader and importer

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-from setuptools import setup
+#!/usr/bin/env python
+import os, sys
+from distutils.core import setup, Extension
 
 setup(
     name='neurograph',
     version='0.0.1',
-    py_modules=['neurograph','lsnimport'],
+    maintainer = "Ivan Raikov",
+    maintainer_email = "ivan.g.raikov@gmail.com",
+    description = "Neurograph library",
+    url = "http://github.com/gheber/neurographdf5",
+    py_modules=['neurograph'],
     include_package_data=True,
     install_requires=[
         'click', 'h5py', 
@@ -13,4 +19,14 @@ setup(
         neurograph=neurograph:cli
         importpairs=importpairs:cli
     ''',
-)
+    ext_modules = [
+        Extension('neurograph_reader',
+                    extra_compile_args = ["-std=c++11"],
+                    libraries = ['hdf5_mpich', 'mpich'],
+                    sources = ['reader/readermodule.cc',
+                               'reader/dbs_graph_reader.cc',
+                               'reader/population_reader.cc'
+                               ])
+        ]
+    )
+
