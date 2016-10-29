25bf51d18bbc86d6af49d550ebdca8371724a5b0
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-10-28 20:32:02 -0700

    bringing python module up to date with attr api

--- reader/src/readermodule.cc
+++ reader/src/readermodule.cc
@@ -4,7 +4,7 @@
 
 #include "dbs_edge_reader.hh"
 #include "population_reader.hh"
-#include "edge_reader.hh"
+#include "graph_reader.hh"
 
 #include <Python.h>
 #include <numpy/numpyconfig.h>
@@ -57,6 +57,7 @@ extern "C"
 
   static PyObject *py_read_graph (PyObject *self, PyObject *args)
   {
+    int status;
     vector<prj_tuple_t> prj_vector;
     vector<string> prj_names;
     PyObject *prj_dict = PyDict_New();
@@ -69,8 +70,11 @@ extern "C"
 
     assert(read_projection_names(MPI_COMM_WORLD, input_file_name, prj_names) >= 0);
 
-    read_graph((void *)(long(commptr)), input_file_name, true,
+    read_graph((MPI_Comm)(long(commptr)), std::string(input_file_name), true,
                prj_names, prj_vector, local_num_edges, total_num_edges);
+
+    printf("prj_vector.size() = %lu\n", prj_vector.size());
+    printf("total_num_edges = %lu\n", total_num_edges);
     
     for (size_t i = 0; i < prj_vector.size(); i++)
       {
@@ -78,89 +82,105 @@ extern "C"
         
         const vector<NODE_IDX_T>& src_vector = get<0>(prj);
         const vector<NODE_IDX_T>& dst_vector = get<1>(prj);
-        
-        const vector<float>&      longitudinal_distance = get<2>(prj);
-        const vector<float>&      transverse_distance   = get<3>(prj);
-        const vector<float>&      distance              = get<4>(prj);
-        const vector<float>&      synaptic_weight       = get<5>(prj);
-        const vector<uint16_t>&   segment_index         = get<6>(prj);
-        const vector<uint16_t>&   segment_point_index   = get<7>(prj);
-        const vector<uint8_t>&    layer                 = get<8>(prj);
-        
-        bool has_longitudinal_distance = longitudinal_distance.size() > 0;
-        bool has_transverse_distance   = transverse_distance.size() > 0;
-        bool has_distance              = distance.size() > 0;
-        bool has_synaptic_weight       = synaptic_weight.size() > 0;
-        bool has_segment_index         = segment_index.size() > 0;
-        bool has_segment_point_index   = segment_point_index.size() > 0;
-        bool has_layer                 = layer.size() > 0;
-        
-        npy_intp dims[1];
-        dims[0] = src_vector.size();
+        const EdgeAttr&  edge_attr_values    = get<2>(prj);
 
+        std::vector <PyObject*> py_float_edge_attrs;
+        std::vector <PyObject*> py_uint8_edge_attrs;
+        std::vector <PyObject*> py_uint16_edge_attrs;
+        std::vector <PyObject*> py_uint32_edge_attrs;
+
+        std::vector <float*> py_float_edge_attrs_ptr;
+        std::vector <uint8_t*> py_uint8_edge_attrs_ptr;
+        std::vector <uint16_t*> py_uint16_edge_attrs_ptr;
+        std::vector <uint32_t*> py_uint32_edge_attrs_ptr;
+        
+        npy_intp dims[1], ind = 0;
+        dims[0] = src_vector.size();
+        
         PyObject *src_arr = PyArray_SimpleNew(1, dims, NPY_UINT32);
         PyObject *dst_arr = PyArray_SimpleNew(1, dims, NPY_UINT32);
-
-        dims[0] = longitudinal_distance.size();
-        PyObject *longitudinal_distance_arr = (PyObject *)PyArray_SimpleNew(1, dims, NPY_FLOAT);
-        dims[0] = transverse_distance.size();
-        PyObject *transverse_distance_arr = (PyObject *)PyArray_SimpleNew(1, dims, NPY_FLOAT);
-        dims[0] = distance.size();
-        PyObject *distance_arr = (PyObject *)PyArray_SimpleNew(1, dims, NPY_FLOAT);
-        dims[0] = synaptic_weight.size();
-        PyObject *synaptic_weight_arr = (PyObject *)PyArray_SimpleNew(1, dims, NPY_FLOAT);
-        dims[0] = segment_index.size();
-        PyObject *segment_index_arr = (PyObject *)PyArray_SimpleNew(1, dims, NPY_UINT16);
-        dims[0] = segment_point_index.size();
-        PyObject *segment_point_index_arr = (PyObject *)PyArray_SimpleNew(1, dims, NPY_UINT16);
-        dims[0] = layer.size();
-        PyObject *layer_arr = (PyObject *)PyArray_SimpleNew(1, dims, NPY_UINT8);
-
-        npy_intp ind = 0;
         uint32_t *src_ptr = (uint32_t *)PyArray_GetPtr((PyArrayObject *)src_arr, &ind);
         uint32_t *dst_ptr = (uint32_t *)PyArray_GetPtr((PyArrayObject *)dst_arr, &ind);
-        float *longitudinal_distance_ptr = (float *)PyArray_GetPtr((PyArrayObject *)longitudinal_distance_arr, &ind);
-        float *transverse_distance_ptr = (float *)PyArray_GetPtr((PyArrayObject *)transverse_distance_arr, &ind);
-        float *distance_ptr = (float *)PyArray_GetPtr((PyArrayObject *)distance_arr, &ind);
-        float *synaptic_weight_ptr = (float *)PyArray_GetPtr((PyArrayObject *)synaptic_weight_arr, &ind);
-        uint16_t *segment_index_ptr = (uint16_t *)PyArray_GetPtr((PyArrayObject *)segment_index_arr, &ind);
-        uint16_t *segment_point_index_ptr = (uint16_t *)PyArray_GetPtr((PyArrayObject *)segment_point_index_arr, &ind);
-        uint8_t *layer_ptr = (uint8_t *)PyArray_GetPtr((PyArrayObject *)layer_arr, &ind);
+        
+        for (size_t j = 0; j < edge_attr_values.size_attr_vec<float>(); j++)
+          {
+            PyObject *arr = (PyObject *)PyArray_SimpleNew(1, dims, NPY_FLOAT);
+            float *ptr = (float *)PyArray_GetPtr((PyArrayObject *)arr, &ind);
+            py_float_edge_attrs.push_back(arr);
+            py_float_edge_attrs_ptr.push_back(ptr);
+          }
+        for (size_t j = 0; j < edge_attr_values.size_attr_vec<uint8_t>(); j++)
+          {
+            PyObject *arr = (PyObject *)PyArray_SimpleNew(1, dims, NPY_UINT8);
+            uint8_t *ptr = (uint8_t *)PyArray_GetPtr((PyArrayObject *)arr, &ind);
+            py_uint8_edge_attrs.push_back(arr);
+            py_uint8_edge_attrs_ptr.push_back(ptr);
+          }
+        for (size_t j = 0; j < edge_attr_values.size_attr_vec<uint16_t>(); j++)
+          {
+            PyObject *arr = (PyObject *)PyArray_SimpleNew(1, dims, NPY_UINT16);
+            uint16_t *ptr = (uint16_t *)PyArray_GetPtr((PyArrayObject *)arr, &ind);
+            py_uint16_edge_attrs.push_back(arr);
+            py_uint16_edge_attrs_ptr.push_back(ptr);
+          }
+        for (size_t j = 0; j < edge_attr_values.size_attr_vec<uint32_t>(); j++)
+          {
+            PyObject *arr = (PyObject *)PyArray_SimpleNew(1, dims, NPY_UINT32);
+            uint32_t *ptr = (uint32_t *)PyArray_GetPtr((PyArrayObject *)arr, &ind);
+            py_uint32_edge_attrs.push_back(arr);
+            py_uint32_edge_attrs_ptr.push_back(ptr);
+          }
         
         
         for (size_t j = 0; j < src_vector.size(); j++)
           {
             src_ptr[j] = src_vector[j];
             dst_ptr[j] = dst_vector[j];
-            if (has_longitudinal_distance)
-              longitudinal_distance_ptr[j] = longitudinal_distance[j];
-            if (has_transverse_distance)
-              transverse_distance_ptr[j] = transverse_distance[j];
-            if (has_distance)
-              distance_ptr[j] = distance[j];
-            if (has_synaptic_weight)
-              synaptic_weight_ptr[j] = synaptic_weight[j];
-            if (has_segment_index)
-              segment_index_ptr[j] = segment_index[j];
-            if (has_segment_point_index)
-              segment_point_index_ptr[j] = segment_point_index[j];
-            if (has_layer)
-              layer_ptr[j] = layer[j];
+            for (size_t k = 0; k < edge_attr_values.size_attr_vec<float>(); k++)
+              {
+                py_float_edge_attrs_ptr[k][j] = edge_attr_values.at<float>(k,j); 
+              }
+            for (size_t k = 0; k < edge_attr_values.size_attr_vec<uint8_t>(); k++)
+              {
+                py_uint8_edge_attrs_ptr[k][j] = edge_attr_values.at<uint8_t>(k,j); 
+              }
+            for (size_t k = 0; k < edge_attr_values.size_attr_vec<uint16_t>(); k++)
+              {
+                py_uint16_edge_attrs_ptr[k][j] = edge_attr_values.at<uint16_t>(k,j); 
+              }
+            for (size_t k = 0; k < edge_attr_values.size_attr_vec<uint32_t>(); k++)
+              {
+                py_uint32_edge_attrs_ptr[k][j] = edge_attr_values.at<uint32_t>(k,j); 
+              }
           }
         
-        PyObject *prjval = PyTuple_Pack(9,
-                                        src_arr,
-                                        dst_arr,
-                                        longitudinal_distance_arr,
-                                        transverse_distance_arr,
-                                        distance_arr,
-                                        synaptic_weight_arr,
-                                        segment_index_arr,
-                                        segment_point_index_arr,
-                                        layer_arr);
+        PyObject *prjval  = PyList_New(0);
+        status = PyList_Append(prjval, src_arr);
+        assert (status == 0);
+        status = PyList_Append(prjval, dst_arr);
+        assert (status == 0);
+        for (size_t j = 0; j < edge_attr_values.size_attr_vec<float>(); j++)
+          {
+            status = PyList_Append(prjval, py_float_edge_attrs[j]);
+            assert(status == 0);
+          }
+        for (size_t j = 0; j < edge_attr_values.size_attr_vec<uint8_t>(); j++)
+          {
+            status = PyList_Append(prjval, py_uint8_edge_attrs[j]);
+            assert(status == 0);
+          }
+        for (size_t j = 0; j < edge_attr_values.size_attr_vec<uint16_t>(); j++)
+          {
+            status = PyList_Append(prjval, py_uint16_edge_attrs[j]);
+            assert(status == 0);
+          }
+        for (size_t j = 0; j < edge_attr_values.size_attr_vec<uint32_t>(); j++)
+          {
+            status = PyList_Append(prjval, py_uint32_edge_attrs[j]);
+            assert(status == 0);
+          }
         
         PyDict_SetItemString(prj_dict, prj_names[i].c_str(), prjval);
-        
         
       }
 
