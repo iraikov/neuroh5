aad35677739ca6c24419db4c1abd4a1818570a2b
Gerd Heber <gheber@hdfgroup.org>
2016-11-08 10:13:44 -0600

    Use H5Literate for more efficient iteration. Cosmetic changes.

--- reader/src/ngh5paths.cc
+++ reader/src/ngh5paths.cc
@@ -13,41 +13,40 @@ using namespace std;
 
 namespace ngh5
 {
-  const string H5PathNames::CONN = "/Connectivity";
+  const string H5PathNames::CONN = "Connectivity";
 
-  const string H5PathNames::POP  = "/Populations";
+  const string H5PathNames::POP  = "Populations";
 
-  const string H5PathNames::PRJ  = "/Projections";
+  const string H5PathNames::PRJ  = "Projections";
 
-  const string H5PathNames::DST_BLK_PTR = "/Connectivity/Destination Block Pointer";
+  const string H5PathNames::DST_BLK_PTR = "Connectivity/Destination Block Pointer";
 
-  const string H5PathNames::DST_IDX = "/Connectivity/Destination Block Index";
+  const string H5PathNames::DST_BLK_IDX = "Connectivity/Destination Block Index";
 
-  const string H5PathNames::DST_PTR = "/Connectivity/Destination Pointer";
+  const string H5PathNames::DST_PTR = "Connectivity/Destination Pointer";
 
-  const string H5PathNames::SRC_IDX = "/Connectivity/Source Index";
+  const string H5PathNames::DST_POP = "Destination Population";
 
-  const string H5PathNames::DST_POP = "/Destination Population";
+  const string H5PathNames::H5_PATH_SEP = "/";
 
-  const string H5PathNames::SRC_POP = "/Source Population";
+  const string H5PathNames::H5_TYPES = "/H5Types";
 
-  const string H5PathNames::POP_RNG = "/Population range";
+  const string H5PathNames::SRC_IDX = "Connectivity/Source Index";
 
-  const string H5PathNames::POP_COMB = "/Valid population projections";
+  const string H5PathNames::SRC_POP = "Source Population";
 
-  string ngh5_prj_path (const string& proj_name, const string& name) 
+  const string H5PathNames::POP_RNG = "Population range";
+
+  const string H5PathNames::POP_COMB = "Valid population projections";
+
+  string ngh5_prj_path (const string& proj_name, const string& name)
   {
-    string result;
-    result = H5PathNames::PRJ + string("/") + proj_name + name;
-    return result;
+    return H5PathNames::PRJ + H5PathNames::H5_PATH_SEP + proj_name + name;
   }
 
-  
-  string ngh5_pop_path (const string& name) 
+  string ngh5_pop_path (const string& name)
   {
-    string result;
-    result = string("/H5Types") + name;
-    return result;
+    return H5PathNames::H5_TYPES + name;
   }
 
 }
