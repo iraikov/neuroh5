ad89a7b57f45880721f6eb3e4c32594d34ab4ec4
Gerd Heber <gheber@hdfgroup.org>
2016-12-15 14:39:30 -0600

    This resolves issue #23.

---
+++ include/io/hdf5/hdf5_enum_type.hh
@@ -0,0 +1,103 @@
+#ifndef HDF5_ENUM_TYPE
+#define HDF5_ENUM_TYPE
+
+#include "hdf5.h"
+
+#include <cassert>
+#include <map>
+#include <string>
+#include <vector>
+
+namespace ngh5
+{
+  namespace io
+  {
+    namespace hdf5
+    {
+      // The datatype handle returned by this function MUST be freed
+      // (via H5Tclose) by the caller or a resource leak will occur.
+
+      template <class T>
+      hid_t create_H5Tenum
+      (
+       const std::vector< std::pair<T, std::string> > enumeration,
+       bool                                           is_signed = true
+       )
+      {
+        hid_t result = -1;
+
+        if (enumeration.size() == 0)
+          {
+            assert("Empty enumeration found.");
+          }
+
+        switch (sizeof(T))
+          {
+          case 1:
+            {
+              if (is_signed)
+                {
+                  result = H5Tenum_create(H5T_STD_I8LE);
+                }
+              else
+                {
+                  result = H5Tenum_create(H5T_STD_U8LE);
+                }
+              break;
+            }
+          case 2:
+            {
+              if (is_signed)
+                {
+                  result = H5Tenum_create(H5T_STD_I16LE);
+                }
+              else
+                {
+                  result = H5Tenum_create(H5T_STD_U16LE);
+                }
+              break;
+            }
+          case 4:
+            {
+              if (is_signed)
+                {
+                  result = H5Tenum_create(H5T_STD_I32LE);
+                }
+              else
+                {
+                  result = H5Tenum_create(H5T_STD_U32LE);
+                }
+              break;
+            }
+          case 8:
+            {
+              if (is_signed)
+                {
+                  result = H5Tenum_create(H5T_STD_I64LE);
+                }
+              else
+                {
+                  result = H5Tenum_create(H5T_STD_U64LE);
+                }
+              break;
+            }
+          default:
+            assert("Unspported type for enumeration base type found.");
+            break;
+          }
+
+        assert(result >= 0);
+
+        for (size_t i = 0; i < enumeration.size(); ++i)
+          {
+            assert(H5Tenum_insert(result, enumeration[i].second.c_str(),
+                                  &enumeration[i].first) >= 0);
+          }
+
+        return result;
+      }
+    }
+  }
+}
+
+#endif
