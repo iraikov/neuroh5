2bfdbc4ec7538f5e27e7cc57e4fd97893c26113a
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-01-18 13:59:06 -0800

    correct names for test data files used in python test scripts

--- tests/reader_test.py
+++ tests/reader_test.py
@@ -1,8 +1,8 @@
 from mpi4py import MPI
-from neurograph.reader import read_graph
+from neurograph.io import read_graph
 
 comm = MPI.COMM_WORLD
 print "rank = ", comm.Get_rank()
 
-g = read_graph(MPI._addressof(comm), "data/dentate_Full_Scale_Control_test2.h5")
+g = read_graph(MPI._addressof(comm), "data/dentate_test.h5")
 
--- tests/scatter_test.py
+++ tests/scatter_test.py
@@ -1,9 +1,9 @@
 from mpi4py import MPI
-from neurograph.reader import scatter_graph
+from neurograph.io import scatter_graph
 
 comm = MPI.COMM_WORLD
 print "rank = ", comm.Get_rank()
 print "size = ", comm.Get_size()
 
-g = scatter_graph(MPI._addressof(comm), "data/dentate_Full_Scale_Control_test2.h5", 1)
+g = scatter_graph(MPI._addressof(comm), "data/dentate_test.h5", 1)
 
