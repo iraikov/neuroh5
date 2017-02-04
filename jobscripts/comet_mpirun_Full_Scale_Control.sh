9827c07697589909e4265ea4f8fbb9199bb185ff
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-02-04 13:59:58 -0800

    added makefile and job scripts for comet

---
+++ jobscripts/comet_mpirun_Full_Scale_Control.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH -J neurograph_scatter_attrs_test
+#SBATCH -o ./results/neurograph_scatter_attrs_test.%j.o
+#SBATCH --nodes=32
+#SBATCH --ntasks-per-node=16
+#SBATCH -p compute
+#SBATCH -t 1:00:00
+#SBATCH --mail-user=ivan.g.raikov@gmail.com
+#SBATCH --mail-type=END
+#
+
+set -x
+
+module load hdf5
+
+ibrun ./build/scatter -a -i 128 \
+ /oasis/scratch/comet/iraikov/temp_project/dentate/Full_Scale_Control/dentate_Full_Scale_Control_MPP.h5
+
+
