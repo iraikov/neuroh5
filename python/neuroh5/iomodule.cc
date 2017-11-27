91440f873b9666b89e112e3a47be732a5a37e2b2
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-11-26 17:59:47 -0800

    harmonized barrier calls at end of append_cell_attributes and write_cel_attributes

--- python/neuroh5/iomodule.cc
+++ python/neuroh5/iomodule.cc
@@ -3277,8 +3277,10 @@ extern "C"
           }
       }
     
-    assert(MPI_Barrier(comm) == MPI_SUCCESS);
+    assert(MPI_Barrier(data_comm) == MPI_SUCCESS);
     assert(MPI_Comm_free(&data_comm) == MPI_SUCCESS);
+    
+    assert(MPI_Barrier(comm) == MPI_SUCCESS);;
     assert(MPI_Comm_free(&comm) == MPI_SUCCESS);
     
     Py_INCREF(Py_None);
