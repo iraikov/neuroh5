2c68fa9ee68d319bb97c583e3bc6066cbf0283d8
Ivan Raikov <ivan.g.raikov@gmail.com>
2018-08-13 08:55:22 -0500

    Resolved merged conflict in tests

--- build/pip_bw.sh
+++ build/pip_bw.sh
@@ -2,5 +2,5 @@
 
 LDCXXSHARED="CC -shared" CXX=CC CC=cc CFLAGS="-DUSE_EDGE_DELIM" MPI_LIB=  \
 HDF5_LIB=hdf5 HDF5_INCDIR=$HDF5_DIR/include HDF5_LIBDIR=$HDF5_DIR/lib \
-pip install -v --upgrade --no-deps --target=/projects/sciteam/baef/site-packages  \
---install-option="--install-scripts=/projects/sciteam/baef/bin"  .
+pip2.7 install -v --upgrade --no-deps --target=/projects/sciteam/baqc/site-packages  \
+--install-option="--install-scripts=/projects/sciteam/baqc/bin"  .
--- tests/test_cell_attr_gen.py
+++ tests/test_cell_attr_gen.py
@@ -1,9 +1,13 @@
 from mpi4py import MPI
-from neuroh5.io import population_ranges, NeuroH5CellAttrGen
+from neuroh5.io import read_population_ranges, NeuroH5TreeGen, NeuroH5CellAttrGen
 
+# import mkl
+import sys
+import os
+import gc
+import numpy as np
 comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-print "rank = ", rank
+rank = comm.rank  # The process ID (integer 0-3 for 4-process run)
 
 #g = NeuroH5CellAttrGen("data/DGC_forest_attr_test_20170614.h5", "GC", io_size=comm.size)
 forest_path = "/oasis/scratch/comet/iraikov/temp_project/dentate/Full_Scale_Control/DGC_forest_syns_compressed_20180425.h5"
@@ -18,4 +22,3 @@ for (gid, synapse_dict) in it:
     if i is not None:
         print 'rank %i: gid = %i' % (rank, i)
 #    print (i, e)
-
