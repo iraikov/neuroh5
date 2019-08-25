fee5cb14b6cf3700130fab5491ec73f451459c68
Ivan Raikov <ivan.g.raikov@gmail.com>
2019-08-25 02:25:01 -0500

    removing -O0 options from setup.py

--- tests/test_scatter_read_trees.py
+++ tests/test_scatter_read_trees.py
@@ -7,4 +7,4 @@ rank = comm.Get_rank()
 (g,n)  = scatter_read_trees("/scratch1/03320/iraikov/dentate/Full_Scale_Control/DG_IN_forest_20190325_compressed.h5", "AAC", io_size=256, comm=comm)
 
 for (gid, tree) in g:
-    print ("rank %d: gid %d = " % (rank, gid, str(tree)))
+    print ("rank %d: gid %d = %s" % (rank, gid, str(tree)))
