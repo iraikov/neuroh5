1ed2c14c7e61f611755ec3bb17405b1905a6550f
Ivan Raikov <ivan.g.raikov@gmail.com>
2019-12-06 16:44:51 -0800

    only enable parallel compression on hdf5 1.10.5+, due to an existing bug in earlier versions

--- src/graph/edge_attributes.cc
+++ src/graph/edge_attributes.cc
@@ -89,7 +89,7 @@ namespace neuroh5
       hid_t plist  = H5Pcreate (H5P_DATASET_CREATE);
       status = H5Pset_chunk(plist, 1, cdims);
       assert(status == 0);
-#if H5_VERSION_GE(1,10,2)
+#ifdef H5_HAS_PARALLEL_DEFLATE
       status = H5Pset_deflate(plist, 6);
       assert(status == 0);
 #endif
