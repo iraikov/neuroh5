a8d2a500fdc67b834c1bcf1b59cd5fd09b68899e
Ivan Raikov <ivan.g.raikov@gmail.com>
2021-10-27 18:18:50 -0500

    version set to 0.1.1

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ setup(
     name='NeuroH5',
     package_dir = {'': 'python'},
     packages = ["neuroh5"],
-    version='0.1.0',
+    version='0.1.1',
     maintainer = "Ivan Raikov",
     maintainer_email = "ivan.g.raikov@gmail.com",
     description = "NeuroH5 library",
