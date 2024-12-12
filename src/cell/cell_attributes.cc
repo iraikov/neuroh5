0a408a37db009031ee40cd7e2303159dee5c5028
Ivan Raikov <ivan.g.raikov@gmail.com>
2024-12-12 08:05:29 -0600

    moved single-rank file creation logic entirely into create_file_toplevel

--- src/cell/cell_attributes.cc
+++ src/cell/cell_attributes.cc
@@ -1847,19 +1847,21 @@ namespace neuroh5
       MPI_Comm_split(comm,color,rank,&io_comm);
       MPI_Comm_set_errhandler(io_comm, MPI_ERRORS_RETURN);
 
+
       if (is_io_rank) {
-        if (access( file_name.c_str(), F_OK ) != 0)
-          {
-            vector <string> groups;
-            groups.push_back (hdf5::POPULATIONS);
-            status = hdf5::create_file_toplevel (io_comm, file_name, groups);
-          }
-        else
-          {
-            status = 0;
-          }
-        throw_assert(status == 0,
-                     "append_cell_attribute_maps: unable to create toplevel groups in file");
+
+          if (access( file_name.c_str(), F_OK ) != 0)
+            {
+              vector <string> groups;
+              groups.push_back (hdf5::POPULATIONS);
+              status = hdf5::create_file_toplevel (io_comm, file_name, groups);
+            }
+          else
+            {
+              status = 0;
+            }
+          throw_assert(status == 0,
+                       "append_cell_attribute_maps: unable to create toplevel groups in file");
       }
       
       throw_assert(MPI_Barrier(comm) == MPI_SUCCESS, "error in MPI_Barrier");
--- src/hdf5/create_file_toplevel.cc
+++ src/hdf5/create_file_toplevel.cc
@@ -23,6 +23,8 @@ namespace neuroh5
     {
       herr_t status;  
       hid_t file, group, prop;
+      // MPI group color value used for I/O rank 0
+      int io_color = 1, color;
       
       int rank, size;
       throw_assert(MPI_Comm_size(comm, &size) == MPI_SUCCESS,
@@ -30,35 +32,46 @@ namespace neuroh5
       throw_assert(MPI_Comm_rank(comm, &rank) == MPI_SUCCESS,
                    "create_file_toplevel: error in MPI_Comm_rank");
       
-      hid_t fapl = H5Pcreate(H5P_FILE_ACCESS);
-      throw_assert(fapl >= 0, "create_file_toplevel: unable to create file access property list");
-#ifdef HDF5_IS_PARALLEL
-      status = H5Pset_fapl_mpio(fapl, comm, MPI_INFO_NULL);
-      throw_assert(status >= 0, "create_file_toplevel: unable to set mpio");
-#endif
+      // MPI Communicator for I/O rank 0
+      MPI_Comm comm_0;
+
+      if (rank == 0) {
+          color = io_color;
+        } else
+          {
+            color = 0;
+          }
+      MPI_Comm_split(comm,color,rank,&comm_0);
+      MPI_Comm_set_errhandler(comm_0, MPI_ERRORS_RETURN);
+
+      if (rank == 0) { 
       
-      /* Create a new file. If file exists its contents will be overwritten. */
-      file = H5Fcreate (file_name.c_str(), H5F_ACC_EXCL, H5P_DEFAULT, fapl);
-      throw_assert(file >= 0, "create_file_toplevel: unable to create file " << file_name);
+	  /* Create a new file. If file exists its contents will be overwritten. */
+	  file = H5Fcreate (file_name.c_str(), H5F_ACC_EXCL, H5P_DEFAULT, H5P_DEFAULT);
+	  throw_assert(file >= 0, "create_file_toplevel: unable to create file " << file_name);
       
-      /* Create dataset creation properties, i.e. to enable chunking  */
-      prop = H5Pcreate (H5P_DATASET_CREATE);
+	  /* Create dataset creation properties, i.e. to enable chunking  */
+	  prop = H5Pcreate (H5P_DATASET_CREATE);
     
-      /* Creates the specified groups.  */
-      for (size_t i=0; i<groups.size(); i++)
-        {
-          group = H5Gcreate2(file, groups[i].c_str(), H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-          throw_assert(group >= 0, "create_file_toplevel: unable to create group " << groups[i]);
-          status = H5Gclose(group);
-          throw_assert(status >= 0, "create_file_toplevel: unable to close group " << groups[i]);
-        }
+	  /* Creates the specified groups.  */
+	  for (size_t i=0; i<groups.size(); i++)
+	    {
+	      group = H5Gcreate2(file, groups[i].c_str(), H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+	      throw_assert(group >= 0, "create_file_toplevel: unable to create group " << groups[i]);
+	      status = H5Gclose(group);
+	      throw_assert(status >= 0, "create_file_toplevel: unable to close group " << groups[i]);
+	    }
       
-      status = H5Pclose (prop);
-      throw_assert(status >= 0, "create_file_toplevel: unable to close property list");
-      status = H5Pclose (fapl);
-      throw_assert(status >= 0, "create_file_toplevel: unable to close property list");
-      status = H5Fclose(file);
-      throw_assert(status >= 0, "create_file_toplevel: unable to close file " << file_name);
+	  status = H5Pclose (prop);
+	  throw_assert(status >= 0, "create_file_toplevel: unable to close property list");
+	  status = H5Fclose(file);
+	  throw_assert(status >= 0, "create_file_toplevel: unable to close file " << file_name);
+      }
+
+      throw_assert(MPI_Barrier(comm) == MPI_SUCCESS, "error in MPI_Barrier");
+      throw_assert(MPI_Comm_free(&comm_0) == MPI_SUCCESS,
+		   "create_file_toplevel: error in MPI_Comm_free");
+
       
       return status;
     }
